f8c43894a5816358a079395076f81b19ed4295ec
P2SAMAPA <pranabpattanaik@gmail.com>
2026-07-14 11:31:01 +0800

    Add new sectors to EQUITY_SECTORS and COMBINED

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ WINDOWS = [63, 252, 504, 1008, 2016, 4032, 4536]
 UNIVERSES = {
     "FI_COMMODITIES": ["TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV"],
     "EQUITY_SECTORS": [
-        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
+        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY", "SMH", "SOXX", "XLB",
         "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "IWM", "IWD", "IWO"
     ],
     "COMBINED": [
         "TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV",
-        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
+        "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY", "SMH", "SOXX", "XLB",
         "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "IWM", "IWD", "IWO"
     ]
 }
